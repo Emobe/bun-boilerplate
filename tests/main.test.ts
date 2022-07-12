39f0318f833446b2c0576ca4dd456c9bd870c8e5
Anthony Shuker <anthony@emobe.uk>
2022-07-12 11:34:39 +0100

    Fix bun test

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,7 +1,9 @@
-import { describe, expect } from "bun:test";
+import { describe, expect, it } from "bun:test";
 import program from '../src/main';
 
-describe('Returns the value passed', () => {
-    const value = 3;
-    expect(program(value)).toBe(3);
+describe('Main program', () => {
+    it('should return 3', () => {
+        const value = 3;
+        expect(program(value)).toBe(value);
+    })
 });
